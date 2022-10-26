78d83b36fdc3f7a12b7849efbd894384c6f22b98
nester256 <nestvlad2004@gmail.com>
2022-10-26 14:41:41 +0300

    Add decorate

--- ln2610.py
+++ ln2610.py
@@ -1,5 +1,8 @@
 # 1.1
 # - найти кол-во дублирующихся букв (не зависит от регистра)
+import time
+
+
 def find_letters(string):
     repeating_l = []
     dublicate_l = []
@@ -15,7 +18,7 @@ def find_letters(string):
     return count
 
 
-print(find_letters('abbccc'))
+# print(find_letters('abbccc'))
 
 
 # 1.2
@@ -23,4 +26,21 @@ def find_letters2(string):
     return len([ch for ch in (set(string.lower())) if string.lower().count(ch) > 1])
 
 
-print(find_letters2('abbccc'))
+# print(find_letters2('abbccc'))
+
+
+def worktime(f1, f2):
+    def f(string):
+        initial_time = time.time()
+        f1(string)
+        first = time.time() - initial_time
+        initial_time = time.time()
+        f2(string)
+        second = time.time() - initial_time
+        print(f1.__name__ if first < second else f2.__name__)
+
+    return f
+
+
+funcion = worktime(find_letters, find_letters2)
+funcion('aaaBBnn')
